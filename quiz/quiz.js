0d04bcbaf2b5317c94259a1624f02e5a4547ea64
parul sharma <parul.sharma@qed42.com>
2017-01-20 14:10:30 +0530

    made some changes in the quiz website

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -6,11 +6,11 @@ var questions=[
   ["Girilal Jain was a noted figure in which of the following fields?","Social Service","Journalism","Literature","Politics","Journalism"],
   ["Who is Tania Sachdev?","chess player","cricketer","actress","dancer","chess player"],
   ["Limba Ram is associated in which fields?","sports","politics","social services","motion picture","sports"],
-  ["Ms. Meera Sahib Fathima Beevi is distinguished as the first lady","Prime minister of Bangladesh","judge of supreme court","Judge of the High court","none","judge of supreme court"],
+  ["Ms. Meera Sahib Fathima Beevi is distinguished as the first lady","PM of Bangladesh","supreme court judge","High court judge","none","supreme court judge"],
   ["Who is the creator of the Rock Garden in Chandigarh?","Pupal Jayakar","Le Corbousier","Glen","Nek Chand","Nek Chand"],
   ["Hitendra Desai was famous in the which field?","Social Service","Journalism","Politics","Literature","Politics"],
   ["Naina Devi, was associated with field of?","Social Service","Stage acting","Classical dance","Vocal music","Vocal music"],
-  ["Who is known as 'Trimurthy of Carnatic Music'?","Muthuswami Dikshitar","Purandardasa","Swami Thirunal","None of these","Muthuswami Dikshitar"],
+  ["Who is known as 'Trimurthy of Carnatic Music'?","Muthuswami","Purandardasa","Swami Thirunal","None of these","Muthuswami"],
   ["Gangubai hangal is the name associated with","Literature","Music","Journalism","Environment","Music"],
   ["Who is known as the 'Lady with the Lamp'?","Florence Nightingale","Sarojini Naidu","Indira Gandhi","Joan of Arc","Florence Nightingale"]
 ]
